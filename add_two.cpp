f9381467e896c9aeced7f2ad15f7d0768e8c22f0
mshakila <mshakila.she@gmail.com>
2023-04-20 15:20:58 +0530

    Add files via upload

---
+++ add_two.cpp
@@ -0,0 +1,12 @@
+include <iostream> 
+using namespace std;
+
+int main(){
+	int a, b, c ;
+	a=10;
+	b=20;
+	c=a+b;
+	cout << "A " << a << " B " << b << " C " << c << endl;
+	cout << "Sum of 2 integers: " << c <<endl;
+	return 0;
+}
